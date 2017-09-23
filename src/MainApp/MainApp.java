81cd0c4599caf91934f407e1f5594ac24589d219
Igor2342 <eclipse2342@gmail.com>
2017-09-23 03:26:56 +0300

    Endless cycle add

--- src/MainApp/MainApp.java
+++ src/MainApp/MainApp.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 
 public class MainApp {
     public static void main(String[] args) throws IOException {
-        DataBase.readBase();
-        System.out.println("finish");
+        while (true) {
+            DataBase.readBase();
+        }
     }
 }
